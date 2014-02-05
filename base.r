5c1468f7c7da3e0040f521b666f65cb7d084aaca
letimome <leticia.montalvillo@ehu.es>
2014-02-05 10:42:48 +0100

    Update base.r

--- base.r
+++ base.r
@@ -2,29 +2,29 @@
 ##Metodo principal
 kdensity<-function(sample,method,n=200,b=length(sample)^0.2){
 	m=min(sample)-2*b
-M=max(sample)+2*b
-x.v=seq(from=m,to=M,by=(M-m)/n)
-#Definir la funcion kernel a utilizar
-kernel=switch(method,
-gauss=gaussian.kernel,
-triangular=triangular.kernel)
-#Obtener la densidad
-dens.v=sapply(x.v,FUN=kernel,sample=sample,b=b)/(length(sample)*b)
-#Resultado final
-list(x=x.v,y=dens.v)
+	M=max(sample)+2*b
+	x.v=seq(from=m,to=M,by=(M-m)/n)
+#	Definir la funcion kernel a utilizar
+	kernel=switch(method,
+	gauss=gaussian.kernel,
+	triangular=triangular.kernel)
+#	Obtener la densidad
+	dens.v=sapply(x.v,FUN=kernel,sample=sample,b=b)/(length(sample)*b)
+#	Resultado final
+	list(x=x.v,y=dens.v)
 }
 
 ##FUNCIONES KERNEL
 ##Funcion triangular
 triangular.kernel=function(x,sample,b){
-s<-sample[abs(sample-x)<=b]
-k<-0
-if (length(s)>0){
-u<-(x-s)/b
-k<-sum(1-abs(u))  
-}
-k
-}
+	s<-sample[abs(sample-x)<=b]
+	k<-0
+	if (length(s)>0){
+		k<-sum(1-abs(u))  
+		u<-(x-s)/b
+	}
+	k
+	}
 
 ##Funcion Gaussiana
 gaussian.kernel=function(x,sample,b){
