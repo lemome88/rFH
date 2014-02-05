f44e011c56d37ae82c75a697f0ea0f9bd60e9885
letimome <leticia.montalvillo@ehu.es>
2014-02-05 09:46:42 +0100

    Update file.r

--- file.r
+++ file.r
@@ -7,22 +7,28 @@ kdensity<-function(sample,method,n=200,b=length(sample)^0.2){
   x.v=seq(from=m,to=M,by=(M-m)/n)
   #Definir la funcion kernel a utilizar
   kernel=switch(method,
-                gauss=gaussian.kernel
-                )
+                gauss=gaussian.kernel,
+                triangular=triangular.kernel)
   #Obtener la densidad
   dens.v=sapply(x.v,FUN=kernel,sample=sample,b=b)/(length(sample)*b)
   #Resultado final
   list(x=x.v,y=dens.v)
 }
 
-
-##Funcion Gaussiana
-gaussian.kernel<-function(x,sample,b){
-  u<-(x-sample)/b
-  sum((1/sqrt(2*3.14159))*exp(-0.5*u^2))
+##FUNCIONES KERNEL
+##Funcion triangular
+triangular.kernel=function(x,sample,b){
+  s<-sample[abs(sample-x)<=b]
+  k<-0
+  if (length(s)>0){
+    u<-(x-s)/b
+    k<-sum(1-abs(u))  
+  }
+  k
 }
 
-##Representacion grafica
-plot.kernel<-function (data,col="red"){
-  plot(data,main="Kernel density estimation",xlab="X",ylab="Density",col=col,type="l",lwd=2)
+##Funcion Gaussiana
+gaussian.kernel=function(x,sample,b){
+  u<-(x-sample)/b
+  sum((1/sqrt(2*3.14159))*exp(-0.5*u^2))
 }
